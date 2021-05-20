9a046b29c00275ab46acb449495f6c9f7907d2e7
Aaron Southammavong <aaronsouthammavong@Aarons-MacBook-Pro.local>
2021-05-20 17:27:28 -0500

    hardcode to dynamic data

---
+++ src/components/EmployeeTable.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="employee-table">
+    <table>
+      <thead>
+        <tr>
+          <th>Employee name</th>
+          <th>Employee email</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Victor Pravo</td>
+          <td>vpravo@vpravo.com</td>
+        </tr>
+        <tr>
+          <td>Rembrandt Rey</td>
+          <td>rrey@rrey.com</td>
+        </tr>
+        <tr>
+          <td>Taka Got</td>
+          <td>tgot@tgot.com</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'employee-table',
+    props: {
+        employees: Array,
+    }
+  }
+</script>
+
+<style scoped></style>
